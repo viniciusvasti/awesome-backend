76e55bd40b35ce89976b95b8f05f115cc9b988cc
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2021-04-18 12:49:29 -0300

    [EXPRESS JS] Handle rest api errors
    Handling errors for:
    - POST with invalid products.
    - GET by SKU when it doesn't exist

--- products-microservice/routes/products.js
+++ products-microservice/routes/products.js
@@ -9,13 +9,27 @@ router.get("/", (req, res) => {
 
 // By appending the RegEx "\d+" to ":sku" path param, it is defining that ":sku" needs to be one or more digits
 router.get("/:sku(\\d+)", (req, res) => {
-  res.send(mockedProducts.filter(({ sku }) => sku === req.params.sku));
+  const { sku } = req.params;
+  const foundEntity = mockedProducts.find(({ sku }) => sku === sku);
+  if (!foundEntity) {
+    res.status(404).send({ message: `Product not found for SKU ${sku}` });
+  } else {
+    res.send(foundEntity);
+  }
 });
 
 router.post("/", (req, res) => {
-  // TODO validation over body
-  mockedProducts = [...mockedProducts, req.body];
-  res.send(mockedProducts);
+  const newProduct = req.body;
+  if (!productIsValid(newProduct)) {
+    res.status(400).send({ message: "Product is not valid" });
+  } else {
+    mockedProducts = [...mockedProducts, newProduct];
+    res.send(mockedProducts);
+  }
 });
+
+function productIsValid(product) {
+  return product.sku && product.name && product.price;
+}
 
 module.exports = router;
