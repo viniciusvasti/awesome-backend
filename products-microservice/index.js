a76c5df2b4d8855c6b9a3773223350c1470c517c
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2021-04-18 11:31:07 -0300

    [EXPRESS JS] Setup express js entry point with products route

--- products-microservice/index.js
+++ products-microservice/index.js
@@ -1,1 +1,11 @@
-console.log("it's working");
+const express = require("express");
+const app = express();
+const productsRouter = require("./routes/products");
+
+const port = 8013;
+
+app.use("/api/v1/product-management/products", productsRouter);
+
+app.listen(port, () => {
+  console.log(`Awesome Backend - Products Microservice is running on ${port}`);
+});
