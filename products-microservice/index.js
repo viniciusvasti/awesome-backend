814489853ed2c50c813ac540017319f9e17f21b1
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2021-04-18 12:02:56 -0300

    [EXPRESS JS] Use custom middleware for logging requests

--- products-microservice/index.js
+++ products-microservice/index.js
@@ -5,6 +5,12 @@ const productsRouter = require("./routes/products");
 const port = 8013;
 
 app.use(express.json());
+app.use("", (req, res, next) => {
+  const requestUri = `Receiving request on ${req.originalUrl}`;
+  const requestBody = `| having body ${JSON.stringify(req.body)}`;
+  console.log(`${requestUri} ${requestBody}`);
+  next();
+});
 app.use("/api/v1/product-management/products", productsRouter);
 
 app.listen(port, () => {
--- products-microservice/routes/products.js
+++ products-microservice/routes/products.js
@@ -3,23 +3,15 @@ const router = express.Router();
 let mockedProducts = require("../mocks/products");
 
 router.get("/", (req, res) => {
-  console.log("Receiving request on", req.originalUrl);
   res.send(mockedProducts);
 });
 
 // By appending the RegEx "\d+" to ":sku" path param, it is defining that ":sku" needs to be one or more digits
 router.get("/:sku(\\d+)", (req, res) => {
-  console.log("Receiving request on", req.originalUrl);
   res.send(mockedProducts.filter(({ sku }) => sku === req.params.sku));
 });
 
 router.post("/", (req, res) => {
-  console.log(
-    "Receiving request on",
-    req.originalUrl,
-    "| having body",
-    req.body
-  );
   // TODO validation over body
   mockedProducts = [...mockedProducts, req.body];
   res.send(mockedProducts);
