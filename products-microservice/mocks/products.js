78f651f3520202a85bb9ab2d05472704c0cfce71
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2021-04-18 11:30:15 -0300

    [EXPRESS JS] Created mocked products list

---
+++ products-microservice/mocks/products.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    sku: "0001",
+    name: "IPhone 12",
+    price: "10000",
+  },
+  {
+    sku: "0002",
+    name: "Samsung S20",
+    price: "8000",
+  },
+  {
+    sku: "0003",
+    name: "Moto Raz4r",
+    price: "2000",
+  },
+];
