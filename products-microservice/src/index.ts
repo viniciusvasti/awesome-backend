b45265f2146352a73a954fc1c70c9c269cca0b82
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2023-06-09 12:28:31 -0300

    Products Microservice - Node TS Setup logging with Pino

--- products-microservice/src/index.ts
+++ products-microservice/src/index.ts
@@ -1,9 +1,34 @@
+import "dotenv/config";
 import Koa from "koa";
+import pinoLogger from "koa-pino-logger";
 
 const app = new Koa();
 app.silent = true; // disable console.errors
 
+const pino = pinoLogger({
+  level: process.env.LOG_LEVEL || "info",
+});
+
 const PORT = process.env.PORT || 8213;
+
+// pino logger
+app.use(pino);
+
+// logger
+app.use(async (ctx, next) => {
+  ctx.log.debug("request received");
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  ctx.log.info(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
 
 // response
 app.use(async (ctx) => {
