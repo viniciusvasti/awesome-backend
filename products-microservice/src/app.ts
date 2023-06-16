020378de9910f7497a4317593f68649ad86aa594
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2023-06-16 11:08:22 -0300

    Separate app setup from running

---
+++ products-microservice/src/app.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import Koa from "koa";
+import pinoLogger from "koa-pino-logger";
+
+const app = new Koa();
+app.silent = true; // disable console.errors
+
+const pino = pinoLogger({
+  level: process.env.LOG_LEVEL || "info",
+});
+
+// pino logger
+app.use(pino);
+
+// Global error handling
+app.on("error", (err, ctx) => {
+  pino.logger.error(err, ctx);
+});
+
+// logging
+app.use(async (ctx, next) => {
+  ctx.log.debug("request received");
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  ctx.log.info(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// response
+app.use(async (ctx) => {
+  ctx.body = "Hello, World!";
+});
+
+export default app;
--- products-microservice/src/index.ts
+++ products-microservice/src/index.ts
@@ -1,44 +1,6 @@
-import "dotenv/config";
-import Koa from "koa";
-import pinoLogger from "koa-pino-logger";
-
-const app = new Koa();
-app.silent = true; // disable console.errors
-
-const pino = pinoLogger({
-  level: process.env.LOG_LEVEL || "info",
-});
+import app from "./app.ts";
 
 const PORT = process.env.PORT || 8213;
-
-// pino logger
-app.use(pino);
-
-// Global error handling
-app.on("error", (err, ctx) => {
-  pino.logger.error(err, ctx);
-});
-
-// logging
-app.use(async (ctx, next) => {
-  ctx.log.debug("request received");
-  await next();
-  const rt = ctx.response.get("X-Response-Time");
-  ctx.log.info(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-
-// x-response-time
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set("X-Response-Time", `${ms}ms`);
-});
-
-// response
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
-});
 
 app.listen(PORT, () => {
   console.log(`Koa is listening to http://localhost:${PORT}`);
